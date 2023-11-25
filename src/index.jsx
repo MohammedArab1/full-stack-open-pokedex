b0b847a557609ef12a4e09dc493c46e565b3ec51
Mohammed Arab <mohammed.arab.1881@gmail.com>
2023-11-25 11:51:30 -0500

    fix eslint errors

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+if (typeof window !== 'undefined') {
+  /* eslint-disable-next-line */
+  ReactDOM.render(<App />, document.getElementById('app'))
+}
